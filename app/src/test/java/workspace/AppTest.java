bed10a6a33fc2e60df4119d826ea17727c8e76ab
kenichi <kn1uji@gmail.com>
2023-08-13 01:06:21 +0000

    learning github actions

---
+++ app/src/test/java/workspace/AppTest.java
@@ -0,0 +1,16 @@
+package workspace;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+  @Test
+  @SuppressWarnings("checkstyle:AbbreviationAsWordInNameCheck")
+  void appHasAGreeting() {
+    App classUnderTest = new App();
+    assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+  }
+
+}
